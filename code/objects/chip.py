47df4d06218c78eabf1e683ab5211b292587feba
timal-99 <18145450@student.hhs.nl>
2021-01-07 22:46:04 +0100

    Klassenupdate

--- code/objects/chip.py
+++ code/objects/chip.py
@@ -1,3 +1,18 @@
+#git branch
+
+#nieuwe branch
+#git branch tim
+
+#ga naar branch
+#git checkout tim
+
+#git pull
+
+#git push main
+#git push origin tijmen:main
+
+from gridpoint import GridPoint
+
 class Chip():
     def __init__(self, width, height):
         self.width = width
@@ -7,6 +22,18 @@ class Chip():
         self.grid = {}
         self.netlist = []
         self.wires = {}
+    
+    def initializeGrid(self):
+        for z in range(self.depth):
+            for y in range(self.height):
+                for x in range(self.width):
+                    if z in self.grid:
+                        if y >= len(self.grid[z]):
+                            self.grid[z].append([GridPoint(x,y,z)])
+                        else:
+                            self.grid[z][y].append(GridPoint(x,y,z))
+                    else:
+                        self.grid[z] = [[GridPoint(x,y,z)]]
 
-
-#test
+    def getGridPoint(self, x, y, z):
+        return self.grid[z][y][x]
--- code/objects/gridpoint.py
+++ code/objects/gridpoint.py
@@ -1,8 +1,8 @@
 class GridPoint():
-    def __init__(self, x, y, z, gate):
+    def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-        self.gate = gate
+        self.gate = False
         self.relatives = []
         self.grid_segments = []
