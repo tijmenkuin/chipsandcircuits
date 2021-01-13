d12abffdcb3501953eed13a24ab5646be52a6dbd
hanan-01 <hanan.almustafa@hotmail.com>
2021-01-13 14:25:26 +0100

    3d plotting

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ from code.algorithms import algorithm1
 if __name__ == "__main__":
     chip = Chip(10,10)
     chip.initializeGates(0)
-    chip.initializeNetList(0,1)
-    chip.getGridPoint(1,3,0).gate_id = 1
-    chip.getGridPoint(2,4,0).gate_id = 2
-    chip.getGridPoint(0,3,0).gate_id = 3
+    # chip.initializeNetList(0,1)
     visualise(chip, 0)
-    print(chip.netlist)
-    # tim.visualise(chip, 0)
-    algorithm1.StartersAlgoritme(chip)
+    # algorithm1.StartersAlgoritme(chip)
