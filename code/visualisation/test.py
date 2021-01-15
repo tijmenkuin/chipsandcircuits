747210b548ddddd37df2d75b65c0f0509226dd22
hanan-01 <hanan.almustafa@hotmail.com>
2021-01-15 15:31:18 +0100

    netlists in 3d visualisation

--- code/visualisation/test.py
+++ code/visualisation/test.py
@@ -1,6 +1,6 @@
 import plotly.express as px
 import plotly.graph_objects as go
-
+import plotly
 
 def visualise(chip):
     X = []
@@ -8,6 +8,16 @@ def visualise(chip):
     Z = []
     Id = []
 
+    #the start and end point for each line
+    # pairs = [(1,5),(2,5),(3,5),(4,5),(5,5),(6,5)]
+
+    x_lines = [1, 2, 3, 4, 5, 6]
+    y_lines = [5, 5, 5, 5, 5, 5]
+    z_lines = [0, 0, 0, 0, 0, 0]
+
+    x_lines1 = [1, 1, 2]
+    y_lines1 = [5, 5, 5]
+    z_lines1 = [2, 3, 3]
     # Create a blank figure with labels
     # df = px.data(X, Y, Z)
 
@@ -19,24 +29,62 @@ def visualise(chip):
                     X.append(x)
                     Z.append(z)
                     Id.append(chip.getGridPoint(x,y,z).gate_id)
+    
+    #create the coordinate list for the lines
+    # for p in pairs:
+    #     for i in range(2):
+    #         x_lines.append(X[p[i]])
+    #         y_lines.append(Y[p[i]])
+    #         z_lines.append(Z[p[i]])
+    
+    gates = go.Scatter3d(
+                        x=X,
+                        y=Y,
+                        z=Z,
+                        # x='Chip Width', y='Chip Height', z='Chip Depth',
+                        surfacecolor='orange', marker_symbol='square',
+                        mode='markers',
+                        text= Id,
+                        name='gates')
 
-    fig = go.Figure(data=[go.Scatter3d(
-        x=X,
-        y=Y,
-        z=Z,
-        marker=dict(
-        size=6,
-        color=z,
-        colorscale='Viridis',
-    ),
-    line=dict(
-        color='darkred',
-        width=2
-    ),
-    marker_symbol='square',
-    text= Id
-        # mode='lines+markers+text', marker_symbol='square'
-    )])
+        # x_lines.append(None)
+        # y_lines.append(None)
+        # z_lines.append(None)
+
+    net_list = go.Scatter3d(
+        x=x_lines,
+        y=y_lines,
+        z=z_lines,
+        mode='lines',
+        name='net_list',surfacecolor='orange'
+    )
+    net_list1 = go.Scatter3d(
+        x=x_lines1,
+        y=y_lines1,
+        z=z_lines1,
+        mode='lines',
+        name='net_list1',surfacecolor='lightblue'
+    )
+
+    # fig = go.Figure(data=[go.Scatter3d(
+    #     x=X,
+    #     y=Y,
+    #     z=Z,
+    #     marker=dict(
+    #     size=6,
+    #     color=z,
+    #     colorscale='Viridis',
+    # ),
+    # line=dict(
+    #     color='darkred',
+    #     width=2
+    # ),
+    # marker_symbol='square',
+    # text= Id
+    #     # mode='lines+markers+text', marker_symbol='square'
+    # )])
+    
+    # fig = go.Figure(data=[go.Scatter3d(x=[3, 4], y=[3, 4], z=[3, 5])])
 
     # for i, txt in enumerate(Id):
 
@@ -63,4 +111,5 @@ def visualise(chip):
 
     # fig = px.scatter_3d(X, Y, Z, x='sepal_length', y='sepal_width', z='petal_width',
     #                     color='petal_length', symbol='species')
+    fig = go.Figure(data=[gates, net_list, net_list1])
     fig.show()
--- testvis.py
+++ testvis.py
@@ -4,7 +4,12 @@ from code.visualisation.test import visualise
 
 chip = Chip(10 ,10)
 chip.initializeGates(0)
-chip.getGridPoint(1,3,0).gate_id = 1
-chip.getGridPoint(2,4,0).gate_id = 2
-chip.getGridPoint(0,3,0).gate_id = 3
+# chip.getGridPoint(1, 5, 0).gate_id = 1
+# chip.getGridPoint(2, 5, 0).gate_id = 2
+# chip.getGridPoint(3, 5, 0).gate_id = 3
+# chip.getGridPoint(4, 5, 0).gate_id = 4
+# chip.getGridPoint(5, 5, 0).gate_id = 5
+# chip.getGridPoint(6, 5, 0).gate_id = 6
+chip.initializeNetlist(0,3)
+
 visualise(chip)
